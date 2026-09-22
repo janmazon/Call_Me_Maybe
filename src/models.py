ffd09ad7ffc7a75f7ab1a57b080094802f79e74c
janmazon <jaaanire@gmail.com>
2026-09-22 17:31:14 +0200

    creadas las clases que heredan de BaseModel

---
+++ src/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Any
+
+
+class ParameterDefinition(BaseModel):
+    type: str
+
+
+class ReturnDefinition(BaseModel):
+    type: str
+
+
+class FunctionDefinition(BaseModel):
+    name: str
+    description: str
+    parameters: dict[str, ParameterDefinition]
+    returns: ReturnDefinition
+
+
+class TestPrompt(BaseModel):
+    prompt: str
+
+
+class FunctionCallResult(BaseModel):
+    prompt: str
+    name: str
+    parameters: dict[str, Any]
